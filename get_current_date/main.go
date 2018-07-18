391482446257223635a17629707ae47a007e7d5f
liviu.1985.esanu@gmail.com <liviu.1985.esanu@gmail.com>
2018-07-18 17:21:07 +0200

    minor refactoring

--- get_current_date/main.go
+++ get_current_date/main.go
@@ -4,15 +4,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/Liviu2018/getCurrentDate/get_current_date/current_date"
 )
 
 var appName = "get current date"
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	current_date.GetCurrentDate()
-	//fmt.Fprintf(w, "%s", current_date.Get_current_date())
+	val := current_date.GetCurrentDate()
+
+	fmt.Fprintf(w, "%s", val)
 }
 
 func main() {
